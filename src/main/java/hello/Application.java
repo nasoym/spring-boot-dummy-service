47cdd98835696536798e14a891296ccf42083100
sinan <sinangoo@gmx.de>
2017-10-30 21:55:04 +0100

    Add logging

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,13 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     @RequestMapping("/")
     public String home() {
+        LOGGER.info("request: /");
         return "Hello Docker World";
     }
 
