5697bd570bfbc383178aaa703a0ba718370a6bfd
sinan <sinangoo@gmx.de>
2017-11-01 22:32:44 +0100

    add new route which uses lots of memory

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.Vector;
 
 @SpringBootApplication
 @RestController
@@ -24,7 +26,20 @@ public class Application {
         for(int i=0;i<=1000;i++) {
           String foo = new String("new String");
         }
-        return "create string";
+        return "ok";
+    }
+
+    @RequestMapping("/memory_usage")
+    public String memory_usage(@RequestParam("usage") int mb_usage) {
+        Vector v = new Vector();
+        for(int i=0; i<mb_usage; i++) {
+          byte[] bytes = new byte[1024*1024];
+          bytes[0] = 0;
+          bytes[1] = 0;
+          bytes[2] = 0;
+          v.add(bytes);
+        }
+        return "ok";
     }
 
     public static void main(String[] args) {
